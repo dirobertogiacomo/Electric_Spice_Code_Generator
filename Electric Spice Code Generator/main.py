77629976ed4807c1355c9cc0f33731d62b6afe25
dirobertogiacomo <102182069+dirobertogiacomo@users.noreply.github.com>
2023-10-28 11:25:24 +0200

    Add files via upload

--- Electric Spice Code Generator/main.py
+++ Electric Spice Code Generator/main.py
@@ -1,9 +1,5 @@
 import functions
 
-ns0 = "((S1 AND S0) OR (S0 AND (NOT A)) OR ((NOT S1) AND (NOT S0) AND A))"
-ns1 = "((NOT S1) AND A) OR (S1  AND (NOT A))"
-Y = "(S0 AND ( ((NOT S1) AND (NOT A)) OR (S1 AND A)) )"
-
 # Input logic function and variables
 logic_function = Y
 input_variables = ["A", "S0", "S1"]
